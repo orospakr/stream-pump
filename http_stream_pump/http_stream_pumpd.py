9983d5b466fa418ff47d46005ee4b9397ab93c05
Andrew Clunis <andrew@orospakr.ca>
2010-11-30 14:54:22 -0500

    #1228. Ported http_stream_pumpd to twisted.web2 (which apparently properly supports the streaming POSTs I need).

--- http_stream_pump/http_stream_pumpd.py
+++ http_stream_pump/http_stream_pumpd.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-from twisted.web import server, resource
-from twisted.internet import reactor
+from twisted.web2 import server, resource, stream, http, http_headers, responsecode, channel, log as weblog
+from twisted.internet import reactor, defer
+from twisted.python import log
+import sys
 
 import logging
-
-logging.basicConfig(level=logging.DEBUG)
 
 def pretty_request(request):
     result = ""
@@ -16,54 +16,82 @@ def pretty_request(request):
 
 def is_header_set_to(request, header, value):
     answer = False
-    for h in request.requestHeaders.getRawHeaders(header):
+    found = request.headers.getRawHeaders(header)
+    if(found is None):
+        return False
+    for h in found:
         if(h == value):
             answer = True
     return answer
 
-class Root(resource.Resource):
-    isLeaf = True
-    def render_GET(self, request):
-        logging.debug("Got request")
-        return "<html>Root!</html>"
+# class Root(resource.Resource):
+#     def render(self, request):
+#         logging.debug("Got request on root")
+#         string = "<html>Root!</html>"
+#         return http.Response(responsecode.OK,
+#                              {'content-type': http_headers.MineType('text', 'html')}, string)
 
-class WMSPushStreamResource(resource.Resource):
-    isLeaf = True
-    def render_POST(self, request):
+
+class WMSPushStreamResource(resource.PostableResource):
+    def http_POST(self, request):
+        """
+        Overloads the herp derp version in web2's resource that fucks up on anything that has
+        a non-typical MIME type.
+
+        We don't want to use the argument-parsing from formencoded feature.
+        """
+        d = defer.succeed(None)
+        d.addCallback(lambda res: self.render(request))
+        return d
+
+    def render(self, request):
         if is_header_set_to(request, "Content-Type", "application/x-wms-pushsetup"):
             # check if already set up, otherwise...
             logging.debug("Got WMS setup pushsetup request!")
-            request.setResponseCode(204) # no content
-            request.responseHeaders.removeHeader("Server") # EE4 gets upset if it thinks it's talking to this thing called "TwistedWeb"...
-            request.responseHeaders.addRawHeader("Server", "Cougar/9.6.7600.16564")
-            request.responseHeaders.addRawHeader("Supported", "com.microsoft.wm.srvppair, com.microsoft.wm.sswitch, com.microsoft.wm.predstrm, com.microsoft.wm.fastcache, com.microsoft.wm.startupprofile")
-            request.responseHeaders.addRawHeader("Cache-Control", "no-cache")
-            request.responseHeaders.addRawHeader("Set-Cookie", "push-id=4242424")
-            return ""
+            answer = http.Response(204, {}, "")
+            answer.headers.setRawHeaders("Supported", ["com.microsoft.wm.srvppair, com.microsoft.wm.sswitch, com.microsoft.wm.predstrm, com.microsoft.wm.fastcache, com.microsoft.wm.startupprofile"])
+            answer.headers.setRawHeaders("Set-Cookie", ["push-id=4242424"])
+            answer.headers.setRawHeaders("Server", ["Cougar/9.6.7600.16564"])
+            answer.headers.setRawHeaders("Cache-Control", ["no-cache"])
+            return answer
         elif is_header_set_to(request, "Content-Type", "application/x-wms-pushstart"):
             logging.debug("A WMS push stream is beginning!")
+
+        return http.Response(422, {}, "")
             
-
 class Base(resource.Resource):
-    def __init__(self):
-        resource.Resource.__init__(self)
-        self.r = Root()
-        self.wms = WMSPushStreamResource()
+    # def __init__(self):
+    #     resource.Resource.__init__(self)
+    #     self.r = Root()
+    #     self.wms = WMSPushStreamResource()
 
-    def getChild(self, name, request):
-        logging.error(pretty_request(request))
-        if(name == ""):
-            return self.r
-        if(name == "mystream"):
-            return self.wms
-        else:
-            logging.debug("404'd!")
-            return resource.NoResource()
+    addSlash = True
 
-    def render_GET(self, request):
+    child_wms = WMSPushStreamResource()
+
+    # def getChild(self, name, request):
+    #     logging.error(pretty_request(request))
+    #     if(name == ""):
+    #         return self.r
+    #     if(name == "mystream"):
+    #         return self.wms
+    #     else:
+    #         logging.debug("404'd!")
+    #         return resource.NoResource()
+
+    def render(self, request):
         logging.debug("Got request")
-        return "<html>Shouldn't see this!</html>"
+        string = "<html>Root!</html>"
+        return http.Response(responsecode.OK,
+                             {'content-type': http_headers.MimeType('text', 'html')}, string)
 
-site = server.Site(Base())
-reactor.listenTCP(8086, site)
-reactor.run()
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    res = Base()
+    site = server.Site(weblog.LogWrapperResource(res))
+    weblog.DefaultCommonAccessLoggingObserver().start()
+    log.startLogging(sys.stderr)
+
+    chan = channel.HTTPFactory(site)
+    reactor.listenTCP(8086, chan)
+    reactor.run()
