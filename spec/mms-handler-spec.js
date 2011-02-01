4524baff7e2990e84f8d1dfeb75bcd3792715a26
Andrew Clunis <andrew@orospakr.ca>
2011-02-01 01:38:27 -0500

    #1640. Test mms-handler with unattached clients.

--- spec/mms-handler-spec.js
+++ spec/mms-handler-spec.js
@@ -3,7 +3,6 @@ var util = require('util');
 var spec_helper = require("./spec_helper.js");
 
 var mms_handler = require('../lib/mms-handler.js');
-
 var mms_stream = require('../lib/mms-stream.js');
 
 
@@ -33,66 +32,112 @@ describe('MMS Handler', function() {
 	expect(received_end).toBeTruthy();
     });
 
-    it("should begin streaming in response to an opened pushstart request", function() {
-	var req = {
-	    headers: {"content-type": "application/x-wms-pushstart"},
-	};
-	var response = {};
+    describe("when stream has started pushing", function() {
+	var stream = undefined;
+	var mock_header = undefined;
+	var header_is_available = undefined;
 
-	var orig_stream = mms_stream.MMSStream;
+	// it should begin streaming...
+	beforeEach(function() {
+	    stream = undefined;
+	    mock_header = { data_length: 42, repack: function() { return "blorp";} };
+	    header_is_available = false;
 
-	var setStreamMock = function(mock) {
-	    this.stream = mock;
-	}.bind(this);
+	    var req = {
+		headers: {"content-type": "application/x-wms-pushstart"},
+	    };
+	    var response = {};
+	    
+	    var orig_stream = mms_stream.MMSStream;
 
-	var constructor_called = false;
+	    var setStreamMock = function(mock) {
+		stream = mock;
+	    }.bind(this);
+	    
+	    var constructor_called = false;
+	    var demuxer_constructor_called = false;
+
+	    mms_stream.MMSStream = function(breq, error_cb) {
+		expect(breq).toBe(req);
+		setStreamMock(this);
+		constructor_called = true;
+
+		this.isHeaderAvailable = function() {
+		    console.log("WELL: " + header_is_available);
+		    return header_is_available;
+		}
+
+		this.getHeader = function() {
+		    return mock_header;
+		};
+	    };
+
+	    handler.consumeRequest(req, response);
+	    
+	    expect(constructor_called).toBeTruthy();
+	    
+	    mms_stream.MMSStream = orig_stream;
+	});
 	
-	mms_stream.MMSStream = function(breq, error_cb) {
-	    expect(breq).toBe(req);
-	    setStreamMock(this);
-	    constructor_called = true;
-	};
-	
-	handler.consumeRequest(req, response);
+	it("should open stream in response to an pushstart request", function() {
+	    // if we got here, success :)
+	});
 
-	expect(constructor_called).toBeTruthy();
+	it("should send a new client an error if stream is not ready", function() {
+	    console.log("CLIENT IS OFF");
+	    var req = {
+		    headers: {}
+	    };
+	    var got_head = false;
+	    var got_end = false;
+	    var response = {
+		writeHead: function(code, headers) {
+		    expect(code).toEqual(503);
+		    got_head = true;
+		},
+		end: function(data) {
+		    expect(got_head).toBeTruthy();
+		    got_end = true;
+		}
+	    };
+	    handler.consumeRequest(req, response);
+	    expect(got_end).toBeTruthy();
+	});
 
-	mms_stream.MMSStream = orig_stream;
+	describe("and when the header packet has arrived", function() {
+	    beforeEach(function() {
+		header_is_available = true;
+	    });
+
+	    it("should send clients the header", function() {
+		console.log("CLIENT IS ON");
+		var req = {
+		    headers: {}
+		};
+		var got_head = false;
+		var got_end = false;
+		var found_content_length_header = false;
+		var response = {
+		    writeHead: function(code, headers) {
+			headers.forEach(function(pair) {
+			    if(pair[0] === "Content-Length") {
+				found_content_length_header = true;
+				expect(pair[1]).toEqual("42");
+			    }
+			});
+			expect(code).toEqual(200);
+			got_head = true;
+		    },
+		    end: function(data) {
+			expect(data).toEqual("blorp");
+			expect(got_head).toBeTruthy();
+			got_end = true;
+		    }
+		};
+		handler.consumeRequest(req, response);
+		expect(got_end).toBeTruthy();
+		expect(found_content_length_header).toBeTruthy();
+	    });    
+	});
     });
 });
-
-
-
-
-derp = {
-    setUp: function(cb) {
-    },
-
-    pushSetup: function(cb) {
-
-    },
-
-    pushStart: function(cb) {
-	
-    },
-
-    clientArrivesAfterPushStart: function(cb) {
-	console.log(util.inspect(this));
-	this.pushStart();
-	var req = {
-	    headers: {}
-	};
-	var response = {
-	    received_head: false,
-	    writeHead: function(code, headers) {
-		cb.equal(200, code);
-		this.received_head = true;
-	    },
-	    end: function(data) {
-		cb.ok(this.received_head);
-		cb.done();
-	    }
-	};
-	this.handler.consumeRequest(req, response);
-    },
-};
