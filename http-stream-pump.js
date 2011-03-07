f0d33b68cdaa30231458a8b3d3471f9547123d6c
Andrew Clunis <andrew@orospakr.ca>
2011-03-06 20:41:19 -0500

    Now configurable.

--- http-stream-pump.js
+++ http-stream-pump.js
@@ -7,11 +7,9 @@
 
 var http = require('http');
 var https = require('https');
-
 var util = require('util');
-
 var url = require('url');
-
+var path = require('path');
 var fs = require('fs');
 
 var strtok = require('strtok');
@@ -19,31 +17,95 @@ var strtok = require('strtok');
 var mmsh_push_source = require('./lib/mmsh-push-source');
 var mmsh_pull_source = require('./lib/mmsh-pull-source');
 var mmsh_handler = require('./lib/mmsh-handler');
+var log = require('./lib/logging');
+
+var c = "Main";
+
+// path.resolve
+
+var config_path = process.argv[2];
+if(config_path === undefined) {
+    console.log("usage: http-stream-pump.js <config file> # see config.js.example");
+    process.exit(-1);
+}
+var config_resolved_path = path.resolve(config_path);
+if(!(path.existsSync(config_resolved_path))) {
+    log.error(c, "Specified config file (" + config_resolved_path + ") does not exist.");
+    process.exit(-1);
+}
+var config = require(config_resolved_path);
+
+var handlers = [];
+
+console.log("Starting up HTTP Stream Pump!");
+
+config.config.streams.forEach(function(strm) {
+    if(!(strm.enabled)) {
+	return;
+    }
+    log.info(c, "Setting up stream: " + strm.name);
+    var failure = function(reason) {
+	log.error(c, "... unable to start stream (" + strm.name + "), because: " + reason);
+    };
+    // TODO test to make sure "path" is a valid URI fragment for HTTP
+    // "^[a-zA-Z0-9_]*$"
+    if (!(strm.path.match(/^[a-zA-Z0-9_]*$/))) {
+	failure("Specified stream path contains inappropriate characters: " + strm.path);
+	return;
+    }
+
+    r = {};
+    if(strm.type === "mmsh_pull") {
+	r.path = strm.path;
+	r.source = new mmsh_pull_source.MMSHPullSource(strm.source_options);
+	r.handler = new mmsh_handler.MMSHHandler(r.source);
+	handlers.push(r);
+    } else if(strm.type === "mmsh_push") {
+	r.path = strm.path;
+	r.source = new mmsh_push_source.MMSHPushSource();
+	r.handler = new mmsh_handler.MMSHHandler(r.source);
+	// add an extra handler for the push source
+	handlers.push(r);
+    } else {
+	log.error(c, "Unknown source type: " + strm.type);
+	return;
+    }
+    log.info(c, "... stream ready!");
+});
+
+if(handlers.length === 0) {
+    log.warn(c, "No streams have been configured.  Idle...");
+}
 
 //var source = new mmsh_push_source.MMSHPushSource();
-var source = new mmsh_pull_source.MMSHPullSource({host:"127.0.0.1", port: "7070", path: "/"});
-var stream_handler = new mmsh_handler.MMSHHandler(source);
+// var source = new mmsh_pull_source.MMSHPullSource({host:"127.0.0.1", port: "7070", path: "/"});
+// var stream_handler = new mmsh_handler.MMSHHandler(source);
 
 var reqHandler = function(req, response) {
     var pathname = url.parse(req.url).pathname;
 
-    console.log(req.method + " " + pathname + " (from: " + req.socket.remoteAddress + "): " + util.inspect(req.headers));
+    log.debug(c, req.method + " " + pathname + " (from: " + req.socket.remoteAddress + "): " + util.inspect(req.headers));
 
-    if(pathname.match(/^\/video/i)) {
-	stream_handler.consumeRequest(req, response);
-    } else if(pathname.match(/^\/videopush/i))  {
-	push_source.consumeRequest(req, response);
-    } else {
+    var hit_handler = false;
+    handlers.forEach(function(handler) {
+	var regex_str = "^\\/streams\\/" + handler.path
+	if(pathname.match(new RegExp(regex_str, "i"))) {
+	    handler.handler.consumeRequest(req, response);
+	    hit_handler = true;
+	}
+    });
+
+    if(!hit_handler) {
 	response.writeHead(404, {"Content-Type": "text/html"});
 
 	response.end("Sorry, nothing here!");
-	console.log("404'd: Attempt to fetch " + pathname);
+	log.warn(c, "404'd: Attempt to fetch " + pathname);
 	
 	return;
     }
 };
 
-console.log("Starting up HTTP Stream Pump!");
+
 
 // var ssl_options = {
 //     key: fs.readFileSync('/home/orospakr/nipcow/self_signed_test/server.key'),
@@ -54,6 +116,6 @@ var serverv4 = http.createServer(reqHandler);
 // var serverv4https = https.createServer(ssl_options, reqHandler);
 // var serverv6 = http.createServer(reqHandler);
 
-serverv4.listen(8080, "0.0.0.0");
+serverv4.listen(config.config.port, "0.0.0.0");
 // serverv4https.listen(8084, "0.0.0.0");
 //serverv6.listen(8086, "::");
