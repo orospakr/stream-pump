4fb997680154a0d30a849a6ad547f937404bd79c
Andrew Clunis <andrew@orospakr.ca>
2011-04-01 15:27:14 -0400

    Fix scoping mistake in load tester; client numbers now show up correctly again.

--- load-tester/load-tester.js
+++ load-tester/load-tester.js
@@ -4,35 +4,36 @@ var http = require('http');
 var util = require('util');
 // "stream2.webdiffusion-webcasting.org"
 
-var options = {host: "localhost",
-	       port: "8085",
+var options = {host: "10.54.201.128",
+	       port: "80",
 	       path: "/streams/test"};
 
-var users = 100;
+var users = 20;
 
 http.getAgent(options.host, options.port).maxSockets = 10000;
 
 var startFetcher = function(me) {
-    console.log("Attaching user #" + me + "!");
-    var req = http.request(options, function(res) {
-	
-	console.log("got connection result for #" + me + "! " + util.inspect(res.headers));
-	res.on('data', function(chunk) {
-    	    // console.log("Got " + chunk.length + " bytes of data.");
+    setTimeout(function() {
+	console.log("Attaching user #" + me + "!");
+	var req = http.request(options, function(res) {
+	    
+	    console.log("got connection result for #" + me + "! " + util.inspect(res.headers));
+	    res.on('data', function(chunk) {
+    		// console.log("Got " + chunk.length + " bytes of data.");
+	    });
+	}).on('error', function(e) {
+	    console.log("got error :( " + e);
 	});
-    }).on('error', function(e) {
-	console.log("got error :( " + e);
-    });
 
-    req.setHeader("User-Agent", "NSPlayer");
-    req.setHeader("Pragma", "xPlayStrm=1");
+	req.setHeader("User-Agent", "NSPlayer");
+	req.setHeader("Pragma", "xPlayStrm=1");
 
-    req.end();
+	req.end();
+    }, 150 * me);
+    
 };
 
 for(i = 0; i < users; i++) {
-    setTimeout(function() {
-	startFetcher(i);	
-    }, 250 * i);
+    startFetcher(i);
 }
 console.log("All stream pullers running!");
