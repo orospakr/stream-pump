c32da44e05ffe109e54385280ac822196d91f5f0
Andrew Clunis <andrew@orospakr.ca>
2011-05-06 15:41:25 -0400

    #1965. Finally, tests for MMSHPullSource.

--- spec/mmsh-pull-source-spec.js
+++ spec/mmsh-pull-source-spec.js
@@ -3,6 +3,117 @@
 // Written by Andrew Clunis <aclunis@credil.org>
 // See COPYING for license terms.
 
+var events = require('events');
+var sys = require('sys');
+var http = require('http');
+var hsp_util = require('../lib/util');
+
+var mmsh_stream = require("../lib/mmsh-stream");
+var mmsh_pull_source = require("../lib/mmsh-pull-source");
+
 describe("MMSH Pull Source", function() {
-    
+    describe("when connecting", function() {
+	beforeEach(function() {
+	    params = {
+		host: "video.ca",
+		port: 80,
+		path: "/"
+	    };
+
+	    orig_config = hsp_util.config;
+	    config = {
+		use_http_proxy: false
+	    };
+	    hsp_util.config = config;
+
+	    orig_hcc = http.createClient;
+	    http_client = new events.EventEmitter();
+
+	    http.createClient = function(connect_port, connect_host) {
+		expect(connect_port).toEqual(80);
+		expect(connect_host).toEqual("video.ca");
+		return http_client;
+	    };
+	    
+	    request = new events.EventEmitter();
+
+	    expected_headers = [
+		{"Pragma": "xPlayStrm=1"},
+		{"User-Agent": "NSPlayer"}
+	    ];
+	    request.setHeader = function(k, v) {
+		expected = expected_headers.pop();
+		expect(expected[k]).toEqual(v);
+	    };
+	    got_end = false;
+	    request.end = function() {
+		expect(expected_headers.length).toEqual(0);
+		got_end = true;
+	    };
+	    
+	    http_client.request = function(method, path, parms) {
+		expect(method).toEqual('GET');
+		expect(path).toEqual('/');
+		return request;
+	    };
+
+	    pull_source = new mmsh_pull_source.MMSHPullSource(params);
+	    expect(got_end).toBeTruthy();
+	});
+
+	it("successfully", function() {
+	    
+	});
+
+	describe(", receives response and sets up stream", function() {
+	    beforeEach(function() {
+		response = {};
+
+		orig_stream = mmsh_stream.MMSHStream;
+		stream = undefined;
+		mmsh_stream.MMSHStream = function(res, incl_preheaders) {
+		    events.EventEmitter.call(this);
+		    expect(incl_preheaders).toBeTruthy();
+		    expect(res).toBe(response);
+		    expect(stream).toBeUndefined();
+		    stream = this;
+		};
+		sys.inherits(mmsh_stream.MMSHStream, events.EventEmitter);
+
+		request.emit('response', response);
+		
+	    });
+
+	    it("successfully", function() {});
+
+	    describe("and stream becomes ready", function() {
+		beforeEach(function() {
+		    got_ready = false;
+		    pull_source.on("ready", function(strm) {
+			expect(strm).toEqual(stream);
+			got_ready = true;
+		    });
+		    stream.emit("ready");
+		    expect(got_ready).toBeTruthy();
+		});
+
+		it("should inform that the stream is ready", function() {});
+	    });
+
+	    afterEach(function() {
+		mmsh_stream.MMSHStream = orig_stream;
+	    });
+	});
+
+	describe("and a connect error happens immediately", function() {
+	    it("successfully stop, and try again", function() {
+		request.emit("error", "nope, no server for you");
+	    });
+	});
+
+	afterEach(function() {
+	    http.createClient = orig_hcc;
+	    hsp_util.config = orig_config;
+	});
+    });
 });
